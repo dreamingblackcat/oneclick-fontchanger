06d32d9b2ffa96f9bf2f3d315edd1422ba0f996f
dreamingblackcat <dreamingblackcat10@gmail.com>
2014-12-16 16:21:36 +0630

    clean the code a bit and added second font setting

--- background.js
+++ background.js
@@ -1,37 +1,30 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
+// Copyright (c) 2012 dreamingblackcat. All rights reserved.
+// Use of this source code is governed by MIT license that can be
 // found in the LICENSE file.
-
+function setFontSettings(storageKey){
+	alert("Key Pressed!");
+	chrome.storage.sync.get(storageKey,function(items){
+		// Change Current Font Setting
+		console.log("Setting is: " + items[storageKey]);
+		chrome.fontSettings.setFont(
+		    { genericFamily: 'sansserif', script: 'Zyyy', fontId: items[storageKey] }
+		  ); 
+		chrome.fontSettings.setFont(
+		    { genericFamily: 'serif', script: 'Zyyy', fontId: items[storageKey] }
+		  );
+		chrome.fontSettings.setFont(
+		    { genericFamily: 'standard', script: 'Zyyy', fontId: items[storageKey] }
+		  );
+	});	
+}
 chrome.commands.onCommand.addListener(function(command) {
-  if (command === "to_zawgyi") {
-  	console.log("Retreving Setting..");
-  	chrome.storage.sync.get('fontId',function(items){
-  		// Change Current Font Setting
-  		console.log("Setting is: " + items.fontId);
-  		chrome.fontSettings.setFont(
-  		    { genericFamily: 'sansserif', script: 'Zyyy', fontId: items.fontId }
-  		  ); 
-  		chrome.fontSettings.setFont(
-  		    { genericFamily: 'serif', script: 'Zyyy', fontId: items.fontId }
-  		  );
-  		chrome.fontSettings.setFont(
-  		    { genericFamily: 'standard', script: 'Zyyy', fontId: items.fontId }
-  		  );
-  	});
+		alert(" key");
+  if(command === "first_font") {
+  	alert("1 key");
+ 	setFontSettings('firstFontId');
     
   }
-  if (command === "to_unicode") {
-  	alert("LDKJFLKDJFL:KJF");
-
-    // Change Current Font Setting
-    chrome.fontSettings.setFont(
-        { genericFamily: 'sansserif', script: 'Zyyy', fontId: 'Noto Sans Myanmar' }
-      ); 
-    chrome.fontSettings.setFont(
-        { genericFamily: 'serif', script: 'Zyyy', fontId: 'Noto Sans Myanmar' }
-      );
-    chrome.fontSettings.setFont(
-        { genericFamily: 'standard', script: 'Zyyy', fontId: 'Noto Sans Myanmar' }
-      );
+  if(command === "second_font") {
+  	setFontSettings('secondFontId');
   }
 });
--- options.js
+++ options.js
@@ -1,28 +1,47 @@
+// Copyright (c) 2012 dreamingblackcat. All rights reserved.
+// Use of this source code is governed by MIT license that can be
+// found in the LICENSE file.
 function save_options() {
   console.log("starting save");
-  var fontId = $('#fonts option:selected').val();
-  console.log(fontId);
+  var firstFontId = $('#first-fonts option:selected').val();
+  var secondFontId = $('#second-fonts option:selected').val();
+  console.log(firstFontId);
   chrome.storage.sync.set({
-    fontId: fontId
+    firstFontId: firstFontId,
+    secondFontId: secondFontId
   }, function() {
     // Update status to let user know options were saved.
-    var status = document.getElementById('status');
     console.log("Saved!");
-    status.textContent = 'Options saved as'+ fontId;
+    $('#status').text('Options saved as'+ firstFontId); 
     setTimeout(function() {
-      status.textContent = '';
+      $('#status').text('');
     }, 750);
+  });
+}
+function restore_last_selection() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    'firstFontId': "Zawgyi-One",
+    'secondFontId': "Noto Sans Myanmar"
+  },function(items) {
+    alert("ITEMS THERE!");
+    console.log(items);
+    $("#first-fonts option[value='"+items.firstFontId+"']").attr("selected",true);
+    $("#second-fonts option[value='"+items.secondFontId+"']").attr("selected",true);
   });
 }
 $(document).ready(function(){
   chrome.fontSettings.getFontList(function(fonts){
-    console.log(fonts);
       $.each(fonts,function(index,font){
-        $('#fonts').append(
+        $('#first-fonts').append(
+          "<option value='"+font.fontId+"'>"+font.displayName+"</option>"
+          );
+        $('#second-fonts').append(
           "<option value='"+font.fontId+"'>"+font.displayName+"</option>"
           );
       });
   });
+  restore_last_selection();
   $("#save").on("click",function(){
     save_options();
   });
@@ -30,13 +49,3 @@ $(document).ready(function(){
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get('fontId', function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    $("#fonts option[value="+items.fontId+"]").attr("selected",true);
-  });
-}
-// document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
