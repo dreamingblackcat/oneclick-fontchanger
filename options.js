e5b8b937c695bb8f0465b99a83a01a5f9ba7bfa1
dreamingblackcat <dreamingblackcat10@gmail.com>
2014-12-19 11:37:54 +0630

    select ui theming and customization started working

--- options.js
+++ options.js
@@ -15,26 +15,54 @@ function save_options() {
     }, 750);
   });
 }
+function first_selection_of($select, value){
+  $select.children('.value')
+    .text(value);
+}
 function restore_last_selection() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
     'firstFontId': "Zawgyi-One",
     'secondFontId': "Noto Sans Myanmar"
   },function(items) {
-    $("#first-fonts option[value='"+items.firstFontId+"']").attr("selected",true);
-    $("#second-fonts option[value='"+items.secondFontId+"']").attr("selected",true);
+      var $first_select_box = $("#first-select .select-container");
+      var $second_select_box = $("#second-select .select-container");
+      first_selection_of($first_select_box, items.firstFontId);
+      first_selection_of($second_select_box, items.secondFontId);
   });
 }
 $(document).ready(function(){
+  $('.fonts_for_select').append($('<div>',{class:"value"}));
+  var $fonts_combo = $('.fonts_for_select');
+  var $ul = $('<ul>',{class:"fonts_combo"});
+  var $month_option = $('<li>',{class:"option"});
   chrome.fontSettings.getFontList(function(fonts){
+    console.log(fonts);
       $.each(fonts,function(index,font){
-        $('#first-fonts').append(
-          "<option value='"+font.fontId+"'>"+font.displayName+"</option>"
-          );
-        $('#second-fonts').append(
-          "<option value='"+font.fontId+"'>"+font.displayName+"</option>"
-          );
+        console.log(font.fontId);
+        console.log($ul);
+        $ul.append($month_option.clone().text(font.fontId));
       });
+  $fonts_combo.append($ul);
+  $('.select_container').click(function() {
+      console.log("noway");
+      $('.fonts_combo').toggle();
+  });
+
+  $('.fonts_combo').children('.option').click(function(){
+    $('.fonts_for_select').children('.value').text($(this).text());
+          console.log("noway");
+
+  });
+
+      // $.each(fonts,function(index,font){
+      //   $('#first-fonts').append(
+      //     "<option value='"+font.fontId+"'>"+font.displayName+"</option>"
+      //     );
+      //   $('#second-fonts').append(
+      //     "<option value='"+font.fontId+"'>"+font.displayName+"</option>"
+      //     );
+      // });
   });
   restore_last_selection();
   $("#save").on("click",function(){
